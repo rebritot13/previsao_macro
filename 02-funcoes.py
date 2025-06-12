6473c817b02f6a70ac3629f98600561dd7728e53
rebritot13 <163076846+rebritot13@users.noreply.github.com>
2025-06-12 12:23:39 -0300

    Update 02-funcoes.py

--- 02-funcoes.py
+++ 02-funcoes.py
@@ -15,6 +15,32 @@ def ler_csv(*args, **kwargs):
   print(f"Falha após {max_tentativas} tentativas.")
   return None
 
+# Separa intervalo de datas em janelas de 10 anos para coleta de dados em blocos
+# na API do BCB/SGS
+def split_date_range(start_date_str, end_date_str, interval_years=5):
+  start_date = datetime.strptime(start_date_str, "%d/%m/%Y")
+  end_date = datetime.strptime(end_date_str, "%d/%m/%Y")
+
+  result = []
+  current_start = start_date
+
+  while current_start < end_date:
+    try:
+      current_end = current_start.replace(year=current_start.year + interval_years)
+    except ValueError:
+      current_end = current_start + timedelta(days=365 * interval_years)
+
+    if current_end > end_date:
+      current_end = end_date
+
+    result.append((
+      current_start.strftime("%d/%m/%Y"),
+      current_end.strftime("%d/%m/%Y")
+    ))
+    current_start = current_end
+
+  return result
+
 # Coleta dados da API do Banco Central (SGS)
 def coleta_bcb_sgs(codigo, nome, freq, data_inicio = "01/01/2000", data_fim = (pd.to_datetime("today") + pd.offsets.DateOffset(months = 36)).strftime("%d/%m/%Y")):
 
